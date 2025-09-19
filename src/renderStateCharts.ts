e0512cdd1dd76d6ad9b4f25dfd2234b0c7bd7041
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2025-09-19 23:33:09 +0200

    chore: update dependencies

--- src/renderStateCharts.ts
+++ src/renderStateCharts.ts
@@ -1,47 +1,47 @@
 import type { FsmProcess } from "@statewalker/fsm";
-import type { StateGraphEdge, StateGraphNode } from "@statewalker/fsm-charts";
 import { isStateTransitionEnabled } from "@statewalker/fsm";
 import { newProcessCharts } from "./newProcessCharts.js";
+import type { StateGraphEdge, StateGraphNode } from "@statewalker/fsm-charts";
 import { _addStateRenderer } from "./_addStateRenderer.js";
 
 export function renderStateCharts({
-  process,
-  direction = "tb",
-  lodash,
-  onStateClick,
-  onEventClick,
-  renderer,
+	process,
+	direction = "tb",
+	lodash,
+	onStateClick,
+	onEventClick,
+	renderer,
 }: {
-  process: FsmProcess;
-  direction?: "tb" | "bt" | "lr" | "rl";
-  lodash: unknown;
-  onStateClick?: (statesStack: StateGraphNode[]) => void;
-  onEventClick?: (edge: StateGraphEdge) => void;
-  renderer?: (statesStack: StateGraphNode[]) => undefined | Node;
+	process: FsmProcess;
+	direction?: "tb" | "bt" | "lr" | "rl";
+	lodash: unknown;
+	onStateClick?: (statesStack: StateGraphNode[]) => void;
+	onEventClick?: (edge: StateGraphEdge) => void;
+	renderer?: (statesStack: StateGraphNode[]) => undefined | Node;
 }) {
-  const charts = newProcessCharts({
-    config: process.config,
-    renderer,
-    onStateClick,
-    onEventClick: async (edge) => {
-      const { event } = edge;
-      if (isStateTransitionEnabled(process, event)) {
-        process.dispatch(event);
-      }
-      if (onEventClick) {
-        onEventClick(edge);
-      }
-    },
-    direction: direction || "tb",
-    lodash,
-  });
-  // const invalidation = getInvalidation(charts);
-  _addStateRenderer(
-    process,
-    (stack) => {
-      return charts.selectState(stack);
-    },
-    // invalidation,
-  );
-  return charts;
+	const charts = newProcessCharts({
+		config: process.config,
+		renderer,
+		onStateClick,
+		onEventClick: async (edge) => {
+			const { event } = edge;
+			if (isStateTransitionEnabled(process, event)) {
+				process.dispatch(event);
+			}
+			if (onEventClick) {
+				onEventClick(edge);
+			}
+		},
+		direction: direction || "tb",
+		lodash,
+	});
+	// const invalidation = getInvalidation(charts);
+	_addStateRenderer(
+		process,
+		(stack) => {
+			return charts.selectState(stack);
+		},
+		// invalidation,
+	);
+	return charts;
 }
--- src/renderStateDescription.ts
+++ src/renderStateDescription.ts
@@ -2,17 +2,17 @@ import type { FsmProcess } from "@statewalker/fsm";
 import { _addStateRenderer } from "./_addStateRenderer.js";
 
 export function renderStateDescription({
-  process,
-  renderer,
+	process,
+	renderer,
 }: {
-  process: FsmProcess;
-  renderer: (statesStack: string[]) => undefined | HTMLElement;
+	process: FsmProcess;
+	renderer: (statesStack: string[]) => undefined | Node;
 }) {
-  const div = document.createElement("div");
-  _addStateRenderer(process, async (stack) => {
-    const view = renderer(stack);
-    view && div.appendChild(view);
-    return () => view?.remove();
-  });
-  return div;
+	const div = document.createElement("div");
+	_addStateRenderer(process, async (stack) => {
+		const view = renderer(stack);
+		view && div.appendChild(view);
+		return () => view?.parentElement?.removeChild(view);
+	});
+	return div;
 }
